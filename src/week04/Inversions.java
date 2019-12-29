bc0ed173d91226d3396ce495ec3d6e5fe716d5ab
nitin023 <khulbe.nitin@gmail.com>
2019-12-29 22:11:35 +0530

    added enhanced merged sort code for finding inversion

--- src/week04/Inversions.java
+++ src/week04/Inversions.java
@@ -3,18 +3,21 @@ package week04;
 import java.util.Scanner;
 
 public class Inversions {
-    private static long getNumberOfInversions(int[] a,  int left, int right) {
+    private static long getNumberOfInversions(int[] a, int[] b, int left, int right) {
         long numberOfInversions = 0;
-        for(int i = left ; i < right ; i++)
-        {
-            for(int j = i + 1 ; j < right ; j++)
-            {
-                if(a[i] > a[j])
-                {
-                    numberOfInversions++;
-                }
-            }
+       int mid;
+        if (right > left) {
+            mid = (right + left) / 2;
+
+            /* Inversion count will be the sum of inversions in left-part, right-part
+          and number of inversions in merging */
+            numberOfInversions += getNumberOfInversions(a, b, left, mid);
+            numberOfInversions += getNumberOfInversions(a, b, mid + 1, right);
+
+            /*Merge the two parts*/
+            numberOfInversions += merge(a, b, left, mid + 1, right);
         }
+        //write your code here
         return numberOfInversions;
     }
 
@@ -25,6 +28,44 @@ public class Inversions {
         for (int i = 0; i < n; i++) {
             a[i] = scanner.nextInt();
         }
-        System.out.println(getNumberOfInversions(a, 0, a.length));
+        int[] b = new int[n];
+        System.out.println(getNumberOfInversions(a, b, 0, a.length));
+    }
+
+
+    private static int merge(int[] arr, int[] temp, int left, int mid, int right)
+    {
+        int i, j, k;
+        int inv_count = 0;
+
+        i = left; /* i is index for left subarray*/
+        j = mid; /* j is index for right subarray*/
+        k = left; /* k is index for resultant merged subarray*/
+        while ((i <= mid - 1) && (j <= right) && j<arr.length && i < arr.length) {
+            if (arr[i] <= arr[j]) {
+                temp[k++] = arr[i++];
+            }
+            else {
+                temp[k++] = arr[j++];
+                /*this is tricky -- see above explanation/diagram for merge()*/
+                inv_count = inv_count + (mid - i);
+            }
+        }
+
+        /* Copy the remaining elements of left subarray
+       (if there are any) to temp*/
+        while (i <= mid - 1)
+            temp[k++] = arr[i++];
+
+        /* Copy the remaining elements of right subarray
+       (if there are any) to temp*/
+        while (j <= right && k<temp.length && j < arr.length)
+            temp[k++] = arr[j++];
+
+        /*Copy back the merged elements to original array*/
+        for (i = left; i <= right && i<arr.length && i < temp.length ; i++)
+            arr[i] = temp[i];
+
+        return inv_count;
     }
 }
